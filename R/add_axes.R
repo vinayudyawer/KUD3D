c634ae61fbfa37d919a87bd72ee4b44942278546
Vinay Udyawer <vinay.udyawer@gmail.com>
2018-12-27 18:03:44 +0930

    Update add_axes.R

--- R/add_axes.R
+++ R/add_axes.R
@@ -29,15 +29,15 @@ add_axes <- function(ras, zscale, axis.col = 1, ...){
 
   ## define axis labs (ll and utm)
   llabx <-
-    pretty(range(raster::coordinates(raster::projectRaster(
-      ras, crs = sp::CRS("+proj=longlat +datum=WGS84")
+    pretty(range(coordinates(projectRaster(
+      ras, crs = CRS("+proj=longlat +datum=WGS84")
     ))[, 1]))
   llaby <-
-    pretty(range(raster::coordinates(raster::projectRaster(
-      ras, crs = sp::CRS("+proj=longlat +datum=WGS84")
+    pretty(range(coordinates(projectRaster(
+      ras, crs = CRS("+proj=longlat +datum=WGS84")
     ))[, 2]))
 
-  labz <- pretty(c(0, min(raster::values(ras))))
+  labz <- pretty(c(0, min(values(ras))))
 
   ## estimate positions for labels
   atx <- seq(-ncol(ras), 0, len=length(llabx))
@@ -45,7 +45,7 @@ add_axes <- function(ras, zscale, axis.col = 1, ...){
   atz <- labz / zscale
 
   ## add axes
-  rgl::bbox3d(xat = aty, xlab = llaby,
+  bbox3d(xat = aty, xlab = llaby,
          yat = atz, ylab = labz,
          zat = atx, zlab = llabx,
          col = axis.col, marklen = 30, ...)
